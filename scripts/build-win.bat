cd025dd6dbfb508fd37163c4b3ea348a9d62bc5c
Sine Striker <trueful@163.com>
2022-11-04 07:01:18 +0800

    Improve CMakeLists

--- scripts/build-win.bat
+++
@@ -1,5 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-
-cmake --no-warn-unused-cli -DAPP_DEPLOY:BOOL=TRUE -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Release -S . -B build -G Ninja
-
-cmake --build build --target all
