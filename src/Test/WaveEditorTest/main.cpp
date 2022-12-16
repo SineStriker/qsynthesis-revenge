f7fa0608e94d034b351b24d925b204b4061d4e57
RigoLigo <rigoligo03@gmail.com>
2022-12-17 01:07:52 +0800

    Improve plugin loading of WaveEditorTest

--- src/Test/WaveEditorTest/main.cpp
+++ src/Test/WaveEditorTest/main.cpp
@@ -1,13 +1,25 @@
 
 #include <QApplication>
 #include "MainWindow.h"
+#ifdef Q_OS_WINDOWS
+    #include <windows.h>
+#endif
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
 
-    w.SetAudio("D:/tmp/song.wav");
+    // Set library loading info
+#ifdef Q_OS_WINDOWS
+    ::SetDllDirectoryW(
+        QString(qApp->applicationDirPath() + "/" + TEST_LIB_DIR).toStdWString().c_str());
+#endif
+    qApp->addLibraryPath(qApp->applicationDirPath() + "/" + TEST_APP_DIR + "/plugins");
+    qApp->addLibraryPath(qApp->applicationDirPath() + "/" + TEST_APP_DIR + "/resources/modules");
+    qApp->addLibraryPath(qApp->applicationDirPath() + "/" + TEST_APP_DIR + "/resources/plugins");
+
+    MainWindow w;
+    w.SetAudio("D:/tmp/song.mp3");
     w.show();
 
     return a.exec();
