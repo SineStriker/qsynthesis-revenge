c990c3dfd5a9fd9ca97a89fa5d64e075389d4c7b
Sine Striker <trueful@163.com>
2023-02-27 23:49:46 +0800

    Temporarily add a splash

--- src/libs/choruskit/appload/main.cpp
+++ src/libs/choruskit/appload/main.cpp
@@ -5,18 +5,107 @@
 #include <QLoggingCategory>
 #include <QMainWindow>
 #include <QPushButton>
+#include <QSplashScreen>
+#include <QTextStream>
 
+#include "QMConsole.h"
+#include "QMSystem.h"
 #include "QMWidgetsHost.h"
 #include "QsCoreConfig.h"
 #include "QsStartInfo.h"
 
 #include "QBreakpadHandler.h"
-
 #include "Windows/PlainWindow.h"
 
-#include "cmdline.h"
+#include "extensionsystem/pluginmanager.h"
 
 extern "C" Q_DECL_EXPORT int main_entry(int argc, char *argv[]);
+
+enum { OptionIndent = 4, DescriptionIndent = 34 };
+
+static const char fixedOptionsC[] = " [options]... [files]...\n";
+
+static const char HELP_OPTION1[] = "-h";
+static const char HELP_OPTION2[] = "--help";
+static const char VERSION_OPTION1[] = "-v";
+static const char VERSION_OPTION2[] = "--version";
+static const char ALLOW_ROOT_OPTION[] = "--allow-root";
+static const char RESET_CONFIG_OPTION[] = "--reset-config";
+static const char PLUGIN_PATH_OPTION[] = "--plugin-path";
+
+static inline void indent(QTextStream &str, int indent) {
+    const QChar blank = QLatin1Char(' ');
+    for (int i = 0; i < indent; i++)
+        str << blank;
+}
+
+static inline void formatOption(QTextStream &str, const QStringList &opts, const QString &parm,
+                                const QString &description) {
+    QString opt = opts.join('/');
+    int remainingIndent = DescriptionIndent - OptionIndent - opt.size();
+    indent(str, OptionIndent);
+    str << opt;
+    if (!parm.isEmpty()) {
+        str << " <" << parm << '>';
+        remainingIndent -= 3 + parm.size();
+    }
+    if (remainingIndent >= 1) {
+        indent(str, remainingIndent);
+    } else {
+        str << '\n';
+        indent(str, DescriptionIndent);
+    }
+    str << description << '\n';
+}
+
+[[maybe_unused]] static inline void displayError(const QString &t) {
+    qmCon->MsgBox(nullptr, QMConsole::Critical, qAppName(), t);
+    QMOs::exitApp(0);
+}
+
+static inline void displayHelpText(const QString &t) {
+    qmCon->MsgBox(nullptr, QMConsole::Information, qAppName(), t);
+    QMOs::exitApp(0);
+}
+
+static inline void printVersion(/*const PluginSpec *coreplugin*/) {
+    QString version;
+    QTextStream str(&version);
+    str << '\n'
+        << qAppName() /*<< ' ' << coreplugin->version()*/ << " based on Qt " << qVersion()
+        << "\n\n";
+    // PluginManager::formatPluginVersions(str);
+    // str << '\n' << coreplugin->copyright() << '\n';
+    displayHelpText(version);
+}
+
+static inline void printHelp() {
+    QString help;
+    QTextStream str(&help);
+
+    str << "Usage: " << qAppName() << fixedOptionsC;
+    formatOption(str, {HELP_OPTION1, HELP_OPTION2}, {}, "Display this help");
+    formatOption(str, {VERSION_OPTION1, VERSION_OPTION2}, {}, "Display application version");
+    formatOption(str,
+                 {
+                     ALLOW_ROOT_OPTION,
+                 },
+                 {}, "Allow running with root privilege");
+    formatOption(str,
+                 {
+                     RESET_CONFIG_OPTION,
+                 },
+                 {}, "Generate or reset configuration file");
+    formatOption(str,
+                 {
+                     PLUGIN_PATH_OPTION,
+                 },
+                 {}, "Add a custom search path for plugins");
+    // ExtensionSystem::PluginManager::formatOptions(str, OptionIndent, DescriptionIndent);
+    // ExtensionSystem::PluginManager::PluginManager::formatPluginOptions(str, OptionIndent,
+    // DescriptionIndent);
+    displayHelpText(help);
+}
 
 int main_entry(int argc, char *argv[]) {
     QApplication a(argc, argv);
@@ -60,7 +149,8 @@ int main_entry(int argc, char *argv[]) {
     // Parse command line
     bool hasHelp = false;
     bool hasVersion = false;
-    QStringList arguments = a.arguments(); // adapted arguments list is passed to plugin manager later
+    QStringList arguments =
+        a.arguments(); // adapted arguments list is passed to plugin manager later
     QMutableStringListIterator it(arguments);
     while (it.hasNext()) {
         const QString &arg = it.next();
@@ -84,13 +174,18 @@ int main_entry(int argc, char *argv[]) {
             }
         }
     }
-    if (hasHelp)
+    if (hasHelp) {
         printHelp();
-    if (hasVersion)
+    }
+    if (hasVersion) {
         printVersion();
+    }
 
     // Prepare to start application
     info.checkLoadInfo();
+
+    QSplashScreen splash(QPixmap(":/svsplash.png"));
+    splash.show();
 
     // Prepare to load plugins
     QBreakpadInstance.setDumpPath(qsConf->appDataDir() + "/crashes");
@@ -99,6 +194,7 @@ int main_entry(int argc, char *argv[]) {
 
     PlainWindow w;
     w.show();
+    splash.finish(&w);
 
     return a.exec();
 }
