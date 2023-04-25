84b0df0a403305442449ec9570ef606279cccc16
CrSjimo <crs_16423@outlook.com>
2023-04-25 18:15:38 +0800

    add time signature structure of MusicTimeManager

--- src/plugins/diffscope/scriptmgr/Internal/JsIntegrationUtils/MusicTimeManager_p.h
+++ src/plugins/diffscope/scriptmgr/Internal/JsIntegrationUtils/MusicTimeManager_p.h
@@ -7,12 +7,16 @@ namespace ScriptMgr::Internal {
 
     class MusicTimeManagerPrivate {
         Q_DECLARE_PUBLIC(MusicTimeManager)
+    public:
+        MusicTimeManagerPrivate();
+        ~MusicTimeManagerPrivate();
         MusicTimeManager *q_ptr;
         int tpqn;
         QMap<int, MusicTimeSignature> timeSignatureMap; //bar position -> time signature
         QMap<int, int> measureMap; //tick position -> bar position
         QMap<int, double> tempoMap; //tick position -> tempo
         QMap<int, double> msecSumMap; //tick position -> msecs in total
+        void updateMeasureMap(int barFrom);
     };
 
 } // Internal
