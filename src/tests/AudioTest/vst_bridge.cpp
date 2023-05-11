34da590332ad767f2602b1d967769017306c2612
CrSjimo <crs_16423@outlook.com>
2023-05-11 13:35:27 +0800

    add test of starting QApplication from vst (not finished)

--- src/tests/AudioTest/vst_bridge.cpp
+++ src/tests/AudioTest/vst_bridge.cpp
@@ -2,6 +2,10 @@
 // Created by Crs_1 on 2023/5/10.
 //
 
+#include <QThread>
+#include <QApplication>
+#include <QWindow>
+
 #include "AudioTrack.h"
 #include "Pcm32BitFloatAudioSource.h"
 #include "VstAudioOutputManager.h"
@@ -14,9 +18,34 @@ AudioTrack *track;
 Pcm32BitFloatAudioSource *src1;
 Pcm32BitFloatAudioSource *src2;
 VstAudioOutputManager *vstMgr;
+QWindow *win;
 
+class GuiThread: public QThread {
+    Q_OBJECT
+    void run() override {
+        if(!QApplication::instance()) {
+            int argc = 0;
+            const char *argv[1];
+            argv[0] = "a";
+            QApplication a(argc, (char**)argv);
+            win = new QWindow;
+            win->setFlag(Qt::WindowStaysOnTopHint, true);
+            connect(this, &GuiThread::showWindow, win, &QWindow::showNormal);
+            connect(this, &GuiThread::hideWindow, win, &QWindow::hide);
+            a.setQuitOnLastWindowClosed(false);
+            a.exec();
+        }
+    }
+signals:
+    void showWindow();
+    void hideWindow();
+};
+
+GuiThread *guiThread;
 
 extern "C" Q_DECL_EXPORT bool Initializer() {
+    guiThread = new GuiThread;
+    guiThread->start();
     track = new AudioTrack(2, 48000);
     src1 = new Pcm32BitFloatAudioSource("D:\\Downloads\\test_source1.pcm", 2, 48000);
     src2 = new Pcm32BitFloatAudioSource("D:\\Downloads\\test_source2.pcm", 2, 48000);
@@ -30,19 +59,20 @@ extern "C" Q_DECL_EXPORT bool Initializer() {
 }
 
 extern "C" Q_DECL_EXPORT bool Terminator() {
-    delete track;
+    track->deleteLater();
     delete src1;
     delete src2;
-    delete vstMgr;
+    vstMgr->deleteLater();
+    guiThread->deleteLater();
     return true;
 }
 
 extern "C" Q_DECL_EXPORT void WindowOpener() {
-
+    emit guiThread->showWindow();
 }
 
 extern "C" Q_DECL_EXPORT void WindowCloser() {
-
+    emit guiThread->hideWindow();
 }
 
 extern "C" Q_DECL_EXPORT bool PlaybackProcessor(const VstAudioOutputManager::PlaybackParameters *playbackParameters, bool isPlaying, int32_t numOutputs, float *const *const *outputs) {
@@ -69,3 +99,5 @@ extern "C" Q_DECL_EXPORT void DirtySetterBinder(void (*setDirty)(bool)) {
 extern "C" Q_DECL_EXPORT bool ProcessInitializer(bool isOffline, double sampleRate) {
     return vstMgr->vstProcessInitializer(isOffline, sampleRate);
 }
+
+#include "vst_bridge.moc"
