f3ddaaf745efc5847b10aa478c67d7941c135377
笛鹿FlutyDeer <68263367+flutydeer@users.noreply.github.com>
2023-08-21 23:27:57 +0800

    Update SeekBar

--- src/libs/qtmediate/base/widgets/Widgets/SeekBar.cpp
+++ src/libs/qtmediate/base/widgets/Widgets/SeekBar.cpp
@@ -25,62 +25,85 @@ SeekBar::SeekBar(QWidget *parent) : QWidget(parent), d(new SeekBarPrivate(this))
         doubleClickLocked = false;
     });
     this->setMinimumWidth(50);
+    setAttribute(Qt::WA_Hover, true);
+    installEventFilter(this);
+    m_thumbHoverAnimation = new QPropertyAnimation;
+    m_thumbHoverAnimation->setTargetObject(this);
+    m_thumbHoverAnimation->setPropertyName("thumbBorderRatio");
+    m_thumbHoverAnimation->setDuration(200);
+    m_thumbHoverAnimation->setEasingCurve(QEasingCurve::OutCubic);
+    calculateParams();
 }
 
 SeekBar::~SeekBar() {
     delete d;
 }
 
+void SeekBar::calculateParams() {
+    m_rect = rect();
+    m_halfHeight = m_rect.height() / 2;
+    m_padding = m_halfHeight;
+    m_trackPenWidth = rect().height() / 3;
+
+    // Calculate slider track
+    m_actualStart = m_rect.left() + m_padding;
+    m_actualEnd = m_rect.right() - m_padding;
+    m_actualLength = m_rect.width() - 2 * m_padding;
+    m_trackStartPoint.setX(m_actualStart);
+    m_trackStartPoint.setY(m_halfHeight);
+    m_trackEndPoint.setX(m_actualEnd);
+    m_trackEndPoint.setY(m_halfHeight);
+
+    // Calculate slider track active
+    m_activeStartPos = int(m_actualLength * (m_trackActiveStartValue - m_min) / (m_max - m_min)) + m_padding;
+    m_activeStartPoint.setX(m_activeStartPos);
+    m_activeStartPoint.setY(m_halfHeight);
+    m_valuePos = int(m_actualLength * (m_value - m_min) / (m_max - m_min)) + m_padding;
+    m_activeEndPoint.setX(m_valuePos);
+    m_activeEndPoint.setY(m_halfHeight);
+}
+
 void SeekBar::paintEvent(QPaintEvent *event) {
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing);
     // Fill background
-    // painter.fillRect(rect(), QColor("#d9d9d9"));
-
-    // Calculate slider track inactive(background)
-    actualLeft = rect().left() + 16;
-    actualRight = rect().right() - 16;
-    actualWidth = rect().width() - 16 - 16;
-    auto trackLeft = QPoint(actualLeft, rect().height() / 2);
-    auto trackRight = QPoint(actualRight, rect().height() / 2);
+    //     painter.fillRect(rect(), QColor("#d0d0d0"));
 
     // Draw slider track inactive(background)
     QPen pen;
-    pen.setColor(QColor(217, 217, 217));
+    pen.setColor(m_trackInactiveColor);
     pen.setCapStyle(Qt::RoundCap);
-    pen.setWidth(8);
+    pen.setWidth(m_trackPenWidth);
     painter.setPen(pen);
-    painter.drawLine(trackLeft, trackRight);
+    painter.drawLine(m_trackStartPoint, m_trackEndPoint);
 
-    // Calculate slider track active
-    auto activeStartPos = int(actualWidth * (m_trackActiveStartValue - m_min) / (m_max - m_min)) + 16;
-    auto activePoint1 = QPoint(activeStartPos, rect().height() / 2);
-    auto valuePos = int(actualWidth * (m_value - m_min) / (m_max - m_min)) + 16;
-    auto activePoint2 = QPoint(valuePos, rect().height() / 2);
+    m_valuePos = int(m_actualLength * (m_value - m_min) / (m_max - m_min)) + m_padding;
+    m_activeEndPoint.setX(m_valuePos);
 
     // Draw slider track active
-    pen.setColor(QColor(112, 156, 255));
-    pen.setWidth(8);
+    pen.setColor(m_trackActiveColor);
+    pen.setWidth(m_trackPenWidth);
     painter.setPen(pen);
-    painter.drawLine(activePoint1, activePoint2);
+    painter.drawLine(m_activeStartPoint, m_activeEndPoint);
 
     // Draw handle
-//    auto handleTopLeft = QPoint(rect().width() / 2 + rect().width() / 3 - 8, rect().height() / 2 - 8);
-//    auto handleBottomRight = QPoint(rect().width() / 2 + rect().width() / 3 + 8, rect().height() / 2 + 8);
-    auto handlePos = activePoint2;
-    auto handleR = 8;
-    pen.setColor(QColor(255, 255, 255));
-    pen.setWidth(4);
+
+    // Calculate handle
+    m_handlePenWidth = m_halfHeight * m_thumbBorderRatio / 255.0;
+    //    qDebug() << m_thumbBorderRatio;
+    m_handleRadius = m_halfHeight - m_handlePenWidth;
+    pen.setColor(m_thumbBorderColor);
+    pen.setWidthF(m_handlePenWidth);
     painter.setPen(pen);
-    painter.setBrush(QColor(112, 156, 255));
-    painter.drawEllipse(handlePos, handleR, handleR);
+    painter.setBrush(m_trackActiveColor);
+    painter.drawEllipse(m_activeEndPoint, m_handleRadius, m_handleRadius);
 
     painter.end();
 }
 
 void SeekBar::setValue(double value) {
     m_value = value;
-    update();
+    repaint();
 }
 
 void SeekBar::setDefaultValue(double value) {
@@ -103,15 +126,15 @@ void SeekBar::reset() {
     update();
     emit valueChanged(m_value);
 }
+
 void SeekBar::mouseMoveEvent(QMouseEvent *event) {
     auto pos = event->pos();
-    if (pos.x() < actualLeft) {
+    if (pos.x() < m_actualStart) {
         setValue(m_min);
         emit valueChanged(m_value);
-    }
-    else if (pos.x() < actualRight) {
-        //        auto valuePos = actualWidth * (m_value - m_min) / (m_max - m_min) + 16;
-        auto posValue = (pos.x() - 16) * (m_max - m_min) / actualWidth + m_min;
+    } else if (pos.x() < m_actualEnd) {
+        //        auto valuePos = m_actualLength * (m_value - m_min) / (m_max - m_min) + 16;
+        auto posValue = (pos.x() - m_padding) * (m_max - m_min) / m_actualLength + m_min;
         setValue(posValue);
         emit valueChanged(m_value);
     } else {
@@ -120,44 +143,120 @@ void SeekBar::mouseMoveEvent(QMouseEvent *event) {
     }
     QWidget::mouseMoveEvent(event);
 }
+
 void SeekBar::mouseDoubleClickEvent(QMouseEvent *event) {
-//    qDebug() << "double click";
+    //    qDebug() << "double click";
     auto pos = event->pos();
     if (mouseOnHandle(pos))
         reset();
     QWidget::mouseDoubleClickEvent(event);
 }
+
 void SeekBar::mousePressEvent(QMouseEvent *event) {
-//    qDebug() << "press";
+    //    qDebug() << "press";
     timer->start();
+    m_thumbHoverAnimation->stop();
+    m_thumbHoverAnimation->setStartValue(m_thumbBorderRatio);
+    m_thumbHoverAnimation->setEndValue(114);
+    m_thumbHoverAnimation->start();
     auto pos = event->pos();
     if (!mouseOnHandle(pos) && !doubleClickLocked) {
-        if (pos.x() >= actualLeft && pos.x() <= actualRight) {
-            //        auto valuePos = actualWidth * (m_value - m_min) / (m_max - m_min) + 16;
-            auto posValue = (pos.x() - 16) * (m_max - m_min) / actualWidth + m_min;
+        auto x = pos.x();
+        if (x < m_actualStart) {
+            setValue(m_min);
+        } else if (x < m_actualEnd) {
+            //        auto valuePos = m_actualLength * (m_value - m_min) / (m_max - m_min) + 16;
+            auto posValue = (pos.x() - m_padding) * (m_max - m_min) / m_actualLength + m_min;
             setValue(posValue);
-            emit valueChanged(m_value);
+        } else {
+            setValue(m_max);
         }
+        emit valueChanged(m_value);
     }
     doubleClickLocked = true;
-    QWidget::mousePressEvent(event);
 }
+
 void SeekBar::mouseReleaseEvent(QMouseEvent *event) {
+    //    qDebug() << "release";
+    m_thumbHoverAnimation->stop();
+    m_thumbHoverAnimation->setStartValue(m_thumbBorderRatio);
+    m_thumbHoverAnimation->setEndValue(77);
+    m_thumbHoverAnimation->start();
     QWidget::mouseReleaseEvent(event);
 }
+
 bool SeekBar::mouseOnHandle(const QPoint &mousePos) const {
     auto pos = mousePos;
-    auto valuePos = actualWidth * (m_value - m_min) / (m_max - m_min) + 16;
-    if (pos.x() >= valuePos - 8 - 4 && pos.x() <= valuePos + 8 + 4) {
+    auto valuePos = m_actualLength * (m_value - m_min) / (m_max - m_min) + m_padding;
+    if (pos.x() >= valuePos - m_halfHeight && pos.x() <= valuePos + m_halfHeight) {
         return true;
     }
     return false;
 }
+
 void SeekBar::setTrackActiveStartValue(double pos) {
     m_trackActiveStartValue = pos;
     update();
 }
+
 void SeekBar::setRange(double min, double max) {
     setMin(min);
     setMax(max);
+}
+
+void SeekBar::resizeEvent(QResizeEvent *event) {
+    calculateParams();
+}
+
+QColor SeekBar::trackInactiveColor() const {
+    return m_trackInactiveColor;
+}
+
+void SeekBar::setTrackInactiveColor(const QColor &color) {
+    m_trackInactiveColor = color;
+    update();
+}
+
+QColor SeekBar::trackActiveColor() const {
+    return m_trackActiveColor;
+}
+
+void SeekBar::setTrackActiveColor(const QColor &color) {
+    m_trackActiveColor = color;
+    update();
+}
+
+QColor SeekBar::thumbBorderColor() const {
+    return m_thumbBorderColor;
+}
+
+void SeekBar::setThumbBorderColor(const QColor &color) {
+    m_thumbBorderColor = color;
+    update();
+}
+
+bool SeekBar::eventFilter(QObject *object, QEvent *event) {
+    if (event->type() == QEvent::HoverEnter) {
+        //        qDebug() << "Hover Enter";
+        m_thumbHoverAnimation->stop();
+        m_thumbHoverAnimation->setStartValue(m_thumbBorderRatio);
+        m_thumbHoverAnimation->setEndValue(77);
+        m_thumbHoverAnimation->start();
+    } else if (event->type() == QEvent::HoverLeave) {
+        //        qDebug() << "Hover Leave";
+        m_thumbHoverAnimation->stop();
+        m_thumbHoverAnimation->setStartValue(m_thumbBorderRatio);
+        m_thumbHoverAnimation->setEndValue(102);
+        m_thumbHoverAnimation->start();
+    }
+    return QObject::eventFilter(object, event);
+}
+
+int SeekBar::thumbBorderRatio() const {
+    return m_thumbBorderRatio;
+}
+
+void SeekBar::setThumbBorderRatio(int ratio) {
+    m_thumbBorderRatio = ratio;
+    update();
 }
--- src/libs/qtmediate/base/widgets/Widgets/SeekBar.h
+++ src/libs/qtmediate/base/widgets/Widgets/SeekBar.h
@@ -8,11 +8,16 @@
 #include "QSlider"
 #include "QMWidgetsGlobal.h"
 #include "QMouseEvent"
+#include <QPropertyAnimation>
 
 class SeekBarPrivate;
 
 class QMWIDGETS_EXPORT SeekBar : public QWidget {
     Q_OBJECT
+    Q_PROPERTY(QColor trackInactiveColor READ trackInactiveColor WRITE setTrackInactiveColor)
+    Q_PROPERTY(QColor trackActiveColor READ trackActiveColor WRITE setTrackActiveColor)
+    Q_PROPERTY(QColor thumbBorderColor READ thumbBorderColor WRITE setThumbBorderColor)
+    Q_PROPERTY(int thumbBorderRatio READ thumbBorderRatio WRITE setThumbBorderRatio)
 
 public:
     explicit SeekBar(QWidget *parent = nullptr);
@@ -30,11 +35,32 @@ signals:
     void valueChanged(double value);
 
 protected:
+    bool eventFilter(QObject *object, QEvent *event) override;
+
     void paintEvent(QPaintEvent *event) override;
+    void resizeEvent(QResizeEvent *event) override;
     void mouseMoveEvent(QMouseEvent *event) override;
     void mouseDoubleClickEvent(QMouseEvent *event) override;
     void mousePressEvent(QMouseEvent *event) override;
     void mouseReleaseEvent(QMouseEvent *event) override;
+
+    void calculateParams();
+    int m_padding = 0;
+    int m_trackPenWidth = 0;
+    QRect m_rect;
+    int m_halfHeight;
+    int m_actualStart = 0;
+    int m_actualEnd = 0;
+    int m_actualLength = 0;
+    QPoint m_trackStartPoint;
+    QPoint m_trackEndPoint;
+    int m_activeStartPos = 0;
+    QPointF m_activeStartPoint;
+    QPointF m_activeEndPoint;
+    int m_valuePos = 0;
+    float m_handlePenWidth = 0;
+    float m_handleRadius = 0;
+
     double m_value = 0;
     double m_defaultValue = 0;
     double m_max = 100;
@@ -43,16 +69,26 @@ protected:
     bool mouseOnHandle(const QPoint &mousePos) const;
     bool handleHover = false;
     bool handlePressed = false;
-    int actualLeft = 0;
-    int actualRight = 0;
-    int actualWidth = 0;
     QTimer *timer;
     bool doubleClickLocked = false;
+    QPropertyAnimation *m_thumbHoverAnimation;
 
 private:
     SeekBarPrivate *d;
+    QColor m_trackInactiveColor = QColor(217, 217, 217);
+    QColor trackInactiveColor() const;
+    void setTrackInactiveColor(const QColor &color);
+    QColor m_trackActiveColor = QColor(112, 156, 255);
+    QColor trackActiveColor() const;
+    void setTrackActiveColor(const QColor &color);
+    QColor m_thumbBorderColor = QColor(255, 255, 255);
+    QColor thumbBorderColor() const;
+    void setThumbBorderColor(const QColor &color);
+
+    // Animation
+    int m_thumbBorderRatio = 102; //ratio max = 255;
+    int thumbBorderRatio() const;
+    void setThumbBorderRatio(int ratio);
 };
-
-
 
 #endif // CHORUSKIT_SEEKBAR_H
